b20f12e7586df7b037623f7f55a6cc8d84da561e
IsResultXaL <774750747@qq.com>
2017-12-05 14:24:53 +0800

    博客详情添加图片悬浮放大

--- src/main/java/com/caogen/blog/config/WebSecurityConfig.java
+++ src/main/java/com/caogen/blog/config/WebSecurityConfig.java
@@ -63,6 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .ignoring()
                 .antMatchers("/css/**",
                         "/editormd/**",
+                        "/zoomify/**",
                         "/index/**",
                         "/image/**",
                         "/js/**",
